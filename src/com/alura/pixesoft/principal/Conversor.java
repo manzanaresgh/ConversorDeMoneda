bc41d03c235aefb2bf6334db25757e710159fd7d
IMPULS\hector.manzanares <hmmg.89@gmail.com>
2025-08-03 23:54:00 -0600

    Update clases

--- src/com/alura/pixesoft/principal/Conversor.java
+++ src/com/alura/pixesoft/principal/Conversor.java
@@ -1,4 +1,87 @@
 package com.alura.pixesoft.principal;
 
+import com.google.gson.*;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Map;
+
 public class Conversor {
+    public double buscarMoneda(int menu, Double valor) {
+        double moneda = 0;
+        String monOrigen = "";
+        switch(menu){
+            case 1,3,5:
+                monOrigen = "USD";
+                break;
+            case 2:
+                monOrigen = "ARS";
+                break;
+            case 4:
+                monOrigen = "BRL";
+                break;
+            case 6:
+                monOrigen = "CLP";
+                break;
+        }
+
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/a3f520380a048ed4ad70e5fd/latest/"+monOrigen);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        //System.out.println(response.body());
+        String json = response.body();
+
+        // Parsear JSON
+        JsonObject root = JsonParser.parseString(json).getAsJsonObject();
+
+        // Extraer solo el objeto conversion_rates
+        JsonObject conversionRates = root.getAsJsonObject("conversion_rates");
+
+        // Convertir a Map<String, Double>
+        Gson gson = new Gson();
+        Type type = new com.google.gson.reflect.TypeToken<Map<String, Double>>(){}.getType();
+        Map<String, Double> tasas = gson.fromJson(conversionRates, type);
+
+        switch(menu){
+            case 1:
+                moneda = valor * tasas.get("ARS");
+                break;
+            case 2:
+                moneda = valor * tasas.get("USD");
+                break;
+            case 3:
+                moneda = valor * tasas.get("BRL");
+                break;
+            case 4:
+                moneda = valor * tasas.get("USD");
+                break;
+            case 5:
+                moneda = valor * tasas.get("CLP");
+                break;
+            case 6:
+                moneda = valor * tasas.get("USD");
+                break;
+            default:
+                moneda = 0;
+        }
+        return moneda;
+    }
 }
--- src/com/alura/pixesoft/principal/Principal.java
+++ src/com/alura/pixesoft/principal/Principal.java
@@ -1,4 +1,71 @@
 package com.alura.pixesoft.principal;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+        Scanner opcion = new Scanner(System.in);
+        Scanner valorAConvertir = new Scanner(System.in);
+        Scanner continuar = new Scanner(System.in);
+        String textoMoneda = "";
+        var op = 0;
+        var val = 0.0;
+        while(true){
+            System.out.println("************************************");
+            System.out.println("Bienvenido al Conversor de Monedas");
+            System.out.println("1) Dólar ==> Peso Argentino.");
+            System.out.println("2) Peso Argentino ==> Dólar.");
+            System.out.println("3) Dólar ==> Real Brazileño.");
+            System.out.println("4) Real Brazileño ==> Dólar.");
+            System.out.println("5) Dólar ==> Peso Colombiano.");
+            System.out.println("6) Peso Colombiano ==> Dólar.");
+            System.out.println("0) Salir.");
+            System.out.println("************************************");
+            System.out.println("Elija la conversión que desea realizar: ");
+            try{
+                op = opcion.nextInt();
+            }catch (Exception e){
+                System.out.println("NO ES UN TIPO DE DATO VÁLIDO");
+                break;
+            }
+
+            System.out.println("************************************");
+            if(op == 1 || op == 2 || op == 3 || op == 4 || op == 5 || op == 6){
+                System.out.println("Agregue valor que desea convertir: ");
+                try{
+                    val = valorAConvertir.nextDouble();
+                }catch (Exception e){
+                    System.out.println("NO ES UN TIPO DE DATO VÁLIDO");
+                    break;
+                }
+
+                System.out.println("************************************");
+                Conversor convertir = new Conversor();
+                String valor = String.valueOf(convertir.buscarMoneda(op, val));
+                if(op == 1){
+                    textoMoneda = "(Peso Argentino)";
+                } else if (op == 2 || op == 4 || op == 6) {
+                    textoMoneda = "(Dolar)";
+                } else if (op == 3) {
+                    textoMoneda = "(Real brasileño)";
+                }else if (op == 5) {
+                    textoMoneda = "(Peso Colombiano)";
+                }else {
+                    break;
+                }
+                System.out.println("Resultado:" +valor+textoMoneda);
+                System.out.println("¿Desea Continuar? SI= 1 NO= 0");
+                var cont = continuar.nextInt();
+                if (cont == 0){
+                    break;
+                }
+            } else if (op == 0) {
+                break;
+            }else{
+                System.out.println("LA OPCIÓN NO ES VÁLIDA");
+            }
+        }
+        System.out.println("Gracias por usar nuestro servicio, ¡hasta pronto! :)");
+    }
 }
